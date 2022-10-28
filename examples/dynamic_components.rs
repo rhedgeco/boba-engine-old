b85c557e3db4d18f69dcd77506303f65b8363802
Ryan Hedgecock <rhedgeco@users.noreply.github.com>
2022-10-28 12:06:40 -0700

    updated example code

--- examples/dynamic_components.rs
+++ examples/dynamic_components.rs
@@ -1,9 +1,12 @@
 use boba_core::*;
 
+#[derive(Debug)]
 struct TestItem1;
 
+#[derive(Debug)]
 struct TestItem2;
 
+#[derive(Debug)]
 struct TestItem3;
 
 struct TestData {
@@ -11,25 +14,29 @@ struct TestData {
 }
 
 impl ControllerStage<TestItem1> for TestData {
-    fn update(&mut self, _: &mut TestItem1, _: &mut BobaResources) {
-        println!("Update 1 data:{:?}", self._data);
+    fn update(&mut self, data: &mut TestItem1, _: &mut BobaResources) {
+        println!("Update with {:?} data:{:?} and increment", data, self._data);
+        self._data += 1;
     }
 }
 
 impl ControllerStage<TestItem2> for TestData {
-    fn update(&mut self, _: &mut TestItem2, _: &mut BobaResources) {
-        println!("Update 2 data:{:?}", self._data);
+    fn update(&mut self, data: &mut TestItem2, _: &mut BobaResources) {
+        println!("Update with {:?} data:{:?}", data, self._data);
     }
 }
 
 register_stages!(TestData: TestItem1, TestItem2);
 
 fn main() {
-    let mut world = BobaApp::default();
+    let mut app = BobaApp::default();
     let test1 = TestData { _data: 5 };
     let cont1 = BobaController::new(test1);
-    world.add_controller(cont1.clone());
-    world.update(&mut TestItem1);
-    world.update(&mut TestItem2);
-    world.update(&mut TestItem3);
+    app.add_controller(cont1.clone());
+    app.update(&mut TestItem1);
+    app.update(&mut TestItem2);
+    app.update(&mut TestItem3);
+    app.update(&mut TestItem1);
+    app.update(&mut TestItem2);
+    app.update(&mut TestItem3);
 }
