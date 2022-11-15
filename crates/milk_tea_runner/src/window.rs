7f86b2c1b1aec1ee52da4c97bba26440875e2a85
Ryan Hedgecock <rhedgeco@users.noreply.github.com>
2022-11-15 11:42:33 -0800

    added ability to apply generic resource to window

--- crates/milk_tea_runner/src/window.rs
+++ crates/milk_tea_runner/src/window.rs
@@ -1,3 +1,5 @@
+use std::any::{Any, TypeId};
+
 use winit::window::Window;
 
 pub struct MilkTeaWindows {
@@ -15,5 +17,60 @@ impl MilkTeaWindows {
 
     pub fn main_mut(&mut self) -> &mut Window {
         &mut self.main
+    }
+}
+
+struct AnySurface {
+    typeid: TypeId,
+    manager: Box<dyn Any>,
+}
+
+pub struct MilkTeaWindow {
+    window: Window,
+    surface: Option<AnySurface>,
+}
+
+impl MilkTeaWindow {
+    pub fn new(window: Window) -> Self {
+        Self {
+            window,
+            surface: None,
+        }
+    }
+
+    pub fn window(&self) -> &Window {
+        &self.window
+    }
+
+    pub fn has_surface<T>(&mut self) -> bool
+    where
+        T: 'static + Sized,
+    {
+        match &self.surface {
+            Some(surface) => surface.typeid == TypeId::of::<T>(),
+            None => false,
+        }
+    }
+
+    pub fn set_surface<T>(&mut self, surface: T) -> &T
+    where
+        T: 'static + Sized,
+    {
+        self.surface = Some(AnySurface {
+            typeid: TypeId::of::<T>(),
+            manager: Box::new(surface),
+        });
+
+        self.get_surface::<T>().unwrap()
+    }
+
+    pub fn get_surface<T>(&mut self) -> Option<&T>
+    where
+        T: 'static + Sized,
+    {
+        match &self.surface {
+            Some(surface) => surface.manager.downcast_ref::<T>(),
+            None => None,
+        }
     }
 }
