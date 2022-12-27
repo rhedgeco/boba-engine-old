894419f580757a26255f473862d910a0a8f3305c
Ryan Hedgecock <rhedgeco@gmail.com>
2022-12-27 15:52:38 -0800

    implemented `BobaResources`

--- crates/boba_core/src/resources.rs
+++ crates/boba_core/src/resources.rs
@@ -1,2 +1,45 @@
+use std::any::{Any, TypeId};
+
+use hashbrown::HashMap;
+
 #[derive(Default)]
-pub struct BobaResources {}
+pub struct BobaResources {
+    resources: HashMap<TypeId, Box<dyn Any>>,
+}
+
+impl BobaResources {
+    pub fn add<T>(&mut self, resource: T) -> Option<T>
+    where
+        T: 'static,
+    {
+        let old_any = self
+            .resources
+            .insert(TypeId::of::<T>(), Box::new(resource))?;
+
+        Some(*old_any.downcast::<T>().unwrap())
+    }
+
+    pub fn get<T>(&self) -> Option<&T>
+    where
+        T: 'static,
+    {
+        let any = self.resources.get(&TypeId::of::<T>())?;
+        Some(any.as_ref().downcast_ref::<T>().unwrap())
+    }
+
+    pub fn get_mut<T>(&mut self) -> Option<&mut T>
+    where
+        T: 'static,
+    {
+        let any = self.resources.get_mut(&TypeId::of::<T>())?;
+        Some(any.as_mut().downcast_mut::<T>().unwrap())
+    }
+
+    pub fn remove<T>(&mut self) -> Option<T>
+    where
+        T: 'static,
+    {
+        let any = self.resources.remove(&TypeId::of::<T>())?;
+        Some(*any.downcast::<T>().unwrap())
+    }
+}
