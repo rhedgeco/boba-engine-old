43122312a01959c20ad36d4f29a4db7c6b875eb0
Ryan Hedgecock <rhedgeco@gmail.com>
2022-12-28 10:15:20 -0800

    added fps example

---
+++ examples/fps.rs
@@ -0,0 +1,32 @@
+use std::time::Instant;
+
+use boba_core::BobaStage;
+use milk_tea::MilkTeaApp;
+
+struct FpsStage {
+    instant: Instant,
+}
+
+impl Default for FpsStage {
+    fn default() -> Self {
+        Self {
+            instant: Instant::now(),
+        }
+    }
+}
+
+impl BobaStage for FpsStage {
+    type Data = ();
+
+    fn run(&mut self, _: &mut boba_core::PearlRegistry, _: &mut boba_core::BobaResources) {
+        let fps = 1. / self.instant.elapsed().as_secs_f64();
+        self.instant = Instant::now();
+        println!("FPS: {fps:.0}");
+    }
+}
+
+fn main() {
+    let mut app = MilkTeaApp::default();
+    app.main_stages.insert(FpsStage::default());
+    app.run().unwrap();
+}
