f39e148a056fd5cdbce741d146d4b938d46e1369
Ryan Hedgecock <rhedgeco@gmail.com>
2022-12-29 15:25:50 -0800

    added a prelude to `boba`

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,18 @@
 pub use boba_core as core;
 pub use milk_tea;
 pub use taro_renderer;
+
+pub mod prelude {
+    pub use milk_tea::MilkTeaApp;
+    pub use taro_renderer::adapters::TaroMilkTea;
+
+    pub use boba_core::BobaResources;
+    pub use boba_core::BobaResult;
+    pub use boba_core::BobaStage;
+    pub use boba_core::Pearl;
+    pub use boba_core::PearlRegistry;
+    pub use boba_core::PearlStage;
+    pub use boba_core::RegisterStages;
+    pub use boba_core::StageRegistrar;
+    pub use boba_core::WrapPearl;
+}
