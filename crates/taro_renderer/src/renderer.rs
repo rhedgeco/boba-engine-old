bbc4974c6b3213792f5a13effbc1192af96ae622
Ryan Hedgecock <rhedgeco@gmail.com>
2022-12-29 18:12:28 -0800

    removed mut requirement from renderer resize

--- crates/taro_renderer/src/renderer.rs
+++ crates/taro_renderer/src/renderer.rs
@@ -1,3 +1,5 @@
+use log::error;
+
 pub struct SurfaceSize {
     pub width: u32,
     pub height: u32,
@@ -11,6 +13,7 @@ pub struct TaroHardware {
 }
 
 pub struct TaroRenderer {
+    initial_config: wgpu::SurfaceConfiguration,
     surface: wgpu::Surface,
     hardware: TaroHardware,
 }
@@ -46,7 +49,7 @@ impl TaroRenderer {
             .await
             .unwrap();
 
-        let config = wgpu::SurfaceConfiguration {
+        let initial_config = wgpu::SurfaceConfiguration {
             usage: wgpu::TextureUsages::RENDER_ATTACHMENT,
             format: surface.get_supported_formats(&adapter)[0],
             width: size.width,
@@ -54,7 +57,7 @@ impl TaroRenderer {
             present_mode: wgpu::PresentMode::Fifo,
             alpha_mode: wgpu::CompositeAlphaMode::Auto,
         };
-        surface.configure(&device, &config);
+        surface.configure(&device, &initial_config);
 
         let hardware = TaroHardware {
             instance,
@@ -63,11 +66,26 @@ impl TaroRenderer {
             queue,
         };
 
-        Self { surface, hardware }
+        Self {
+            initial_config,
+            surface,
+            hardware,
+        }
     }
 
-    pub fn surface(&self) -> &wgpu::Surface {
-        &self.surface
+    pub fn resize_surface(&self, new_size: SurfaceSize) {
+        if new_size.width == 0 && new_size.height == 0 {
+            error!(
+                "Could not set TaroRenderer surface size to ({},{}).Width and height must be greater than 0.",
+                new_size.width, new_size.height
+            );
+            return;
+        }
+
+        let mut new_config = self.initial_config.clone();
+        new_config.width = new_size.width;
+        new_config.height = new_size.height;
+        self.surface.configure(&self.hardware.device, &new_config);
     }
 
     pub fn hardware(&self) -> &TaroHardware {
