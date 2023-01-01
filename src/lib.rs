29b1643bf1714eab3adc4e550b2b82a7314f61ff
Ryan Hedgecock <rhedgeco@gmail.com>
2022-12-31 18:23:04 -0800

    add `taro_adapters` as main boba dependency

--- src/lib.rs
+++ src/lib.rs
@@ -1,9 +1,10 @@
 pub use boba_core as core;
 pub use milk_tea;
+pub use taro_adapters;
 pub use taro_renderer;
 
 pub mod prelude {
     pub use boba_core::*;
     pub use milk_tea::Bobarista;
-    pub use taro_renderer::adapters::milk_tea::*;
+    pub use taro_adapters::milk_tea::TaroMilkTea;
 }
