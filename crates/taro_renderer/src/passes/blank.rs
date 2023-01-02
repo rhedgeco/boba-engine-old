05eac0cf1b2fb48635b78d981cd23eb186d4818a
Ryan Hedgecock <rhedgeco@gmail.com>
2023-01-01 21:58:08 -0800

    made the blank pass solid alpha black

--- crates/taro_renderer/src/passes/blank.rs
+++ crates/taro_renderer/src/passes/blank.rs
@@ -23,7 +23,7 @@ impl TaroRenderPass for BlankRenderPass {
                         r: 0.0,
                         g: 0.0,
                         b: 0.0,
-                        a: 0.0,
+                        a: 1.0,
                     }),
                     store: true,
                 },
