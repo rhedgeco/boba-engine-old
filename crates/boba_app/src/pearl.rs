a29e65b310da3ef1c0858015b054b1bf05cfd030
Ryan Hedgecock <rhedgeco@gmail.com>
2023-01-29 16:04:29 -0800

    expose `PearlCore` for shared api usage

--- crates/boba_app/src/pearl.rs
+++ crates/boba_app/src/pearl.rs
@@ -1,14 +1,33 @@
 use std::{
     cell::{BorrowError, BorrowMutError, Ref, RefCell, RefMut},
     hash::Hash,
+    ops::Deref,
     rc::Rc,
 };
 
 use crate::BobaId;
 
-struct InnerPearl<T> {
+/// The internal structure that is shared between [`Pearl`] and [`PearlLink`]
+pub struct PearlCore<T> {
     id: BobaId,
     data: RefCell<T>,
+}
+
+impl<T> PearlCore<T> {
+    /// Gets the [`BobaId`] for this pearl
+    pub fn id(&self) -> &BobaId {
+        &self.id
+    }
+
+    /// Borrows the underlying data from this pearl
+    pub fn borrow(&self) -> Result<Ref<T>, BorrowError> {
+        self.data.try_borrow()
+    }
+
+    /// Mutably borrows the underlying data from this pearl
+    pub fn borrow_mut(&self) -> Result<RefMut<T>, BorrowMutError> {
+        self.data.try_borrow_mut()
+    }
 }
 
 /// Core struct for wrapping data to connected to a node
@@ -17,7 +36,36 @@ struct InnerPearl<T> {
 /// Instead, the pearl can be built into a [`PearlLink`] which has essentially the same functionality, but can be cloned and used across anywhere it is needed.
 /// However, links cannot be added directly to a node.
 pub struct Pearl<T> {
-    inner: Rc<InnerPearl<T>>,
+    core: Rc<PearlCore<T>>,
+}
+
+impl<T> Deref for Pearl<T> {
+    type Target = PearlCore<T>;
+
+    fn deref(&self) -> &Self::Target {
+        &self.core
+    }
+}
+
+impl<T> Pearl<T> {
+    /// Creates a new `Pearl` containing `data`
+    pub fn new(data: T) -> Self {
+        let inner = PearlCore {
+            id: BobaId::new(),
+            data: RefCell::new(data),
+        };
+
+        Self {
+            core: Rc::new(inner),
+        }
+    }
+
+    /// Creates a new [`PearlLink`] for this pearl
+    pub fn new_link(&self) -> PearlLink<T> {
+        PearlLink {
+            core: self.core.clone(),
+        }
+    }
 }
 
 /// A link to the core [`Pearl`] struct.
@@ -25,72 +73,27 @@ pub struct Pearl<T> {
 /// This can be cloned, and should be used when data needs to be linked across pearls.
 #[derive(Clone)]
 pub struct PearlLink<T> {
-    inner: Rc<InnerPearl<T>>,
+    core: Rc<PearlCore<T>>,
 }
 
 impl<T> Eq for PearlLink<T> {}
 
 impl<T> PartialEq for PearlLink<T> {
     fn eq(&self, other: &Self) -> bool {
-        self.inner.id == other.inner.id
+        self.core.id == other.core.id
     }
 }
 
 impl<T> Hash for PearlLink<T> {
     fn hash<H: std::hash::Hasher>(&self, state: &mut H) {
-        self.inner.id.hash(state);
+        self.core.id.hash(state);
     }
 }
 
-impl<T> Pearl<T> {
-    /// Creates a new `Pearl` containing `data`
-    pub fn new(data: T) -> Self {
-        let inner = InnerPearl {
-            id: BobaId::new(),
-            data: RefCell::new(data),
-        };
+impl<T> Deref for PearlLink<T> {
+    type Target = PearlCore<T>;
 
-        Self {
-            inner: Rc::new(inner),
-        }
-    }
-
-    /// Gets the [`BobaId`] for this pearl
-    pub fn id(&self) -> &BobaId {
-        &self.inner.id
-    }
-
-    /// Creates a new [`PearlLink`] for this pearl
-    pub fn new_link(&self) -> PearlLink<T> {
-        PearlLink {
-            inner: self.inner.clone(),
-        }
-    }
-
-    /// Borrows the underlying data from this pearl
-    pub fn borrow(&self) -> Result<Ref<T>, BorrowError> {
-        self.inner.data.try_borrow()
-    }
-
-    /// Mutably borrows the underlying data from this pearl
-    pub fn borrow_mut(&self) -> Result<RefMut<T>, BorrowMutError> {
-        self.inner.data.try_borrow_mut()
-    }
-}
-
-impl<T> PearlLink<T> {
-    /// Gets the [`BobaId`] for the [`Pearl`] this link points to
-    pub fn id(&self) -> &BobaId {
-        &self.inner.id
-    }
-
-    /// Borrows the underlying data from the [`Pearl`] this link points to
-    pub fn borrow(&self) -> Result<Ref<T>, BorrowError> {
-        self.inner.data.try_borrow()
-    }
-
-    /// Mutably borrows the underlying data from the [`Pearl`] this link points to
-    pub fn borrow_mut(&self) -> Result<RefMut<T>, BorrowMutError> {
-        self.inner.data.try_borrow_mut()
+    fn deref(&self) -> &Self::Target {
+        &self.core
     }
 }
