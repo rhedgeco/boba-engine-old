885f2590a4bd5b3a697fb27f3c22f0cbc2666b97
Ryan Hedgecock <rhedgeco@gmail.com>
2023-01-31 14:33:15 -0800

    improved `Node` and added pearl references

--- crates/boba_world/src/node.rs
+++ crates/boba_world/src/node.rs
@@ -1,4 +1,8 @@
-use crate::{Handle, HandleMap, HandleResult};
+use std::any::{Any, TypeId};
+
+use indexmap::IndexMap;
+
+use crate::{Handle, HandleMap, Pearl, PearlHandle, World};
 
 /// A map containing [`Node`] objects
 pub type NodeMap<const ID: usize> = HandleMap<Node<ID>, ID>;
@@ -13,6 +17,7 @@ pub type NodeHandle<const ID: usize> = Handle<Node<ID>, ID>;
 pub struct Node<const ID: usize> {
     parent: Option<NodeHandle<ID>>,
     children: Vec<NodeHandle<ID>>,
+    pearls: IndexMap<TypeId, Box<dyn Any>>,
 }
 
 impl<const ID: usize> Node<ID> {
@@ -48,19 +53,44 @@ impl<const ID: usize> Node<ID> {
     ///
     /// ## Warning
     /// Using a the wrong map for this node is ***undefined behaviour***, and may result in a panic
-    pub fn get_parent<'a>(&'a self, map: &'a NodeMap<ID>) -> Option<&Node<ID>> {
+    pub fn get_parent<'a>(&'a self, world: &'a World<ID>) -> Option<&Node<ID>> {
         let parent = self.parent.as_ref()?;
-        map.get(parent).ok()
+        world.nodes.get(parent).ok()
     }
 
     /// Gets the children for this node using the given `map`
     ///
     /// ## Warning
     /// Using a the wrong map for this node is ***undefined behaviour***, and may result in a panic
-    pub fn get_children<'a>(&'a self, map: &'a NodeMap<ID>) -> HandleResult<Vec<&Node<ID>>> {
+    pub fn get_children<'a>(&'a self, world: &'a World<ID>) -> Vec<&Node<ID>> {
         let child_iter = self.children.iter();
-        Ok(child_iter
-            .filter_map(|h| map.get(h).ok())
-            .collect::<Vec<_>>())
+        child_iter
+            .filter_map(|h| world.nodes.get(h).ok())
+            .collect::<Vec<_>>()
+    }
+
+    /// Gets a reference to the [`Pearl`] containing with `T` from this given node.
+    /// Returns `None` if the pearl does not exist.
+    ///
+    /// ## Warning
+    /// Using a the wrong map for this node is ***undefined behaviour***, and may result in a panic
+    pub fn get_pearl<'a, T: 'static>(&'a self, world: &'a World<ID>) -> Option<&Pearl<T>> {
+        let handle = self.pearls.get(&TypeId::of::<T>())?;
+        let handle = handle.downcast_ref::<PearlHandle<T, ID>>().unwrap();
+        world.pearls.get(handle).ok()
+    }
+
+    /// Gets a mutable reference to the [`Pearl`] containing with `T` from this given node.
+    /// Returns `None` if the pearl does not exist.
+    ///
+    /// ## Warning
+    /// Using a the wrong map for this node is ***undefined behaviour***, and may result in a panic
+    pub fn get_pearl_mut<'a, T: 'static>(
+        &'a self,
+        world: &'a mut World<ID>,
+    ) -> Option<&mut Pearl<T>> {
+        let handle = self.pearls.get(&TypeId::of::<T>())?;
+        let handle = handle.downcast_ref::<PearlHandle<T, ID>>().unwrap();
+        world.pearls.get_mut(handle).ok()
     }
 }
