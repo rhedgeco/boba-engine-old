bfdadbce8c5d2794a0603f7e961760d43463ddd5
Ryan Hedgecock <rhedgeco@gmail.com>
2023-01-30 18:52:00 -0800

    added struct for managing event listeners

---
+++ crates/boba_app/src/event.rs
@@ -0,0 +1,82 @@
+use std::any::{type_name, Any, TypeId};
+
+use indexmap::{map::Entry, IndexMap};
+use log::error;
+
+use crate::{BobaId, BobaResult, Pearl};
+
+pub trait OnEvent<Data>: Sized + 'static {
+    fn update(pearl: &Pearl<Self>, data: &Data) -> BobaResult;
+}
+
+pub trait EventRegistrar {
+    fn add_listener<Data: 'static>(&mut self, pearl: &Pearl<impl OnEvent<Data>>);
+}
+
+pub trait RegisterEvents: Sized + 'static {
+    fn register(pearl: &Pearl<Self>, registrar: &mut impl EventRegistrar);
+}
+
+trait EventListener<Data>: 'static {
+    fn update(&self, data: &Data);
+}
+
+impl<Data, Event: OnEvent<Data>> EventListener<Data> for Pearl<Event> {
+    /// Event listener implementation for pearls
+    fn update(&self, data: &Data) {
+        if let Err(e) = Event::update(self, data) {
+            let update_name = type_name::<Data>();
+            let pearl_name = type_name::<Event>();
+            error!("There was an error while running event `{update_name}` on Pearl<{pearl_name}>. Error: {e}");
+        }
+    }
+}
+
+struct EventSet<Data> {
+    listeners: IndexMap<BobaId, Box<dyn EventListener<Data>>>,
+}
+
+impl<Data> EventSet<Data> {
+    pub fn new() -> Self {
+        Self {
+            listeners: IndexMap::new(),
+        }
+    }
+
+    pub fn add_listener(&mut self, pearl: &Pearl<impl OnEvent<Data>>) {
+        self.listeners
+            .insert(pearl.id().clone(), Box::new(pearl.clone()));
+    }
+}
+
+#[derive(Default)]
+pub struct EventListenerCollection {
+    event_sets: IndexMap<TypeId, Box<dyn Any>>,
+}
+
+impl EventRegistrar for EventListenerCollection {
+    fn add_listener<Data: 'static>(&mut self, pearl: &Pearl<impl OnEvent<Data>>) {
+        let typeid = TypeId::of::<Data>();
+        match self.event_sets.entry(typeid) {
+            Entry::Occupied(e) => {
+                let set = e.into_mut().downcast_mut::<EventSet<Data>>().unwrap();
+                set.add_listener(pearl)
+            }
+            Entry::Vacant(e) => {
+                let mut set = EventSet::<Data>::new();
+                set.add_listener(pearl);
+                e.insert(Box::new(set));
+            }
+        }
+    }
+}
+
+impl EventListenerCollection {
+    pub fn new() -> Self {
+        Self::default()
+    }
+
+    pub fn register_pearl<T: RegisterEvents>(&mut self, pearl: &Pearl<T>) {
+        T::register(pearl, self);
+    }
+}
--- crates/boba_app/src/lib.rs
+++ crates/boba_app/src/lib.rs
@@ -1,8 +1,11 @@
+mod event;
 mod id;
 mod node;
 mod pearl;
 
 use anyhow::Result;
+
+pub use event::*;
 pub use id::*;
 pub use node::*;
 pub use pearl::*;
