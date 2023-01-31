9b04f7d12ab49d101bd33399ba902bdaddd8f1ae
Ryan Hedgecock <rhedgeco@gmail.com>
2023-01-31 14:39:18 -0800

    make inner maps public for `World`

--- crates/boba_world/src/world.rs
+++ crates/boba_world/src/world.rs
@@ -2,8 +2,8 @@ use crate::{NodeMap, PearlMap};
 
 #[derive(Default)]
 pub struct World<const ID: usize> {
-    pub(crate) nodes: NodeMap<ID>,
-    pub(crate) pearls: PearlMap<ID>,
+    pub nodes: NodeMap<ID>,
+    pub pearls: PearlMap<ID>,
 }
 
 impl<const ID: usize> World<ID> {
