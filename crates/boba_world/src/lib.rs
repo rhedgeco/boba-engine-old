00f60cc8445f358257db700bb89320db50c558df
Ryan Hedgecock <rhedgeco@gmail.com>
2023-02-02 12:48:52 -0800

    removed `node.rs` and reexported public API items

--- crates/boba_world/src/lib.rs
+++ crates/boba_world/src/lib.rs
@@ -1,7 +1,6 @@
 mod event;
 mod handle;
 mod id;
-mod node;
 mod pearl;
 mod world;
 
@@ -10,9 +9,11 @@ use anyhow::Result;
 pub use event::*;
 pub use handle::*;
 pub use id::*;
-pub use node::*;
 pub use pearl::*;
 pub use world::*;
 
 /// A type alias to [`anyhow::Result`]
 pub type BobaResult = Result<()>;
+
+// re-export public API items
+pub use indexmap;
