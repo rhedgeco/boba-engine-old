ac20bd8e3c595d0ca5a11255687504963e8d6a85
Ryan Hedgecock <rhedgeco@gmail.com>
2023-02-14 11:35:53 -0800

    improved and added tests for pearls suite

--- crates/boba_ecs/src/pearl.rs
+++ crates/boba_ecs/src/pearl.rs
@@ -5,7 +5,7 @@ use indexmap::{IndexMap, IndexSet};
 
 use crate::EntityId;
 
-#[derive(Eq)]
+#[derive(Eq, Clone)]
 pub struct PearlTypes {
     types: IndexSet<TypeId>,
 }
@@ -46,11 +46,11 @@ impl PearlTypes {
         self.types.is_empty()
     }
 
-    pub fn is_superset(&self, other: &PearlTypes) -> bool {
-        other.is_subset(self)
+    pub fn is_subset_of(&self, other: &PearlTypes) -> bool {
+        other.is_superset_of(self)
     }
 
-    pub fn is_subset(&self, other: &PearlTypes) -> bool {
+    pub fn is_superset_of(&self, other: &PearlTypes) -> bool {
         // if this set has less elements that the other set
         // it could not possibly be a subset
         if self.types.len() < other.types.len() {
@@ -96,8 +96,9 @@ impl PearlTypesBuilder {
         Self::default()
     }
 
-    pub fn add_type<T: 'static>(&mut self) {
+    pub fn add_type<T: 'static>(mut self) -> Self {
         self.types.insert(TypeId::of::<T>());
+        self
     }
 
     pub fn build(mut self) -> PearlTypes {
@@ -161,8 +162,9 @@ impl PearlSetBuilder {
     }
 
     #[inline]
-    pub fn add<T: 'static>(&mut self, pearl: T) {
+    pub fn add<T: 'static>(mut self, pearl: T) -> Self {
         self.pearls.insert(TypeId::of::<T>(), Imposter::new(pearl));
+        self
     }
 
     pub fn build(mut self) -> PearlSet {
@@ -246,5 +248,209 @@ impl Archetype {
         }
 
         return true;
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use crate::Entities;
+
+    use super::*;
+
+    struct Type1(u16);
+    struct Type2(u32);
+    struct Type3(u64);
+    struct Type4(u128);
+
+    #[test]
+    fn pearl_types_sorted() {
+        let types1 = PearlTypesBuilder::new()
+            .add_type::<Type1>()
+            .add_type::<Type2>()
+            .add_type::<Type3>()
+            .build();
+
+        let types2 = PearlTypesBuilder::new()
+            .add_type::<Type2>()
+            .add_type::<Type3>()
+            .add_type::<Type1>()
+            .build();
+
+        for i in 0..3 {
+            let type1 = types1.types.get_index(i).unwrap();
+            let type2 = types2.types.get_index(i).unwrap();
+            assert!(type1 == type2);
+        }
+    }
+
+    #[test]
+    fn pearl_set_sorted() {
+        let set1 = PearlSetBuilder::new()
+            .add(Type1(1))
+            .add(Type2(2))
+            .add(Type3(3))
+            .build();
+
+        let set2 = PearlSetBuilder::new()
+            .add(Type2(1))
+            .add(Type3(2))
+            .add(Type1(3))
+            .build();
+
+        assert!(set2.types() == set1.types());
+
+        for i in 0..3 {
+            let type1 = set1.types.types.get_index(i).unwrap();
+            let (pearltype1, data1) = set1.pearls.get_index(i).unwrap();
+            let type2 = set2.types.types.get_index(i).unwrap();
+            let (pearltype2, data2) = set2.pearls.get_index(i).unwrap();
+            assert!(&data1.type_id() == type1);
+            assert!(&data2.type_id() == type2);
+            assert!(type1 == type2);
+            assert!(pearltype1 == pearltype2);
+        }
+    }
+
+    #[test]
+    fn types_subset() {
+        let types1 = PearlTypesBuilder::new()
+            .add_type::<Type1>()
+            .add_type::<Type2>()
+            .add_type::<Type3>()
+            .build();
+
+        let types2 = PearlTypesBuilder::new()
+            .add_type::<Type2>()
+            .add_type::<Type1>()
+            .build();
+
+        let types3 = PearlTypesBuilder::new()
+            .add_type::<Type2>()
+            .add_type::<Type1>()
+            .add_type::<Type4>()
+            .build();
+
+        assert!(types2.is_subset_of(&types1));
+        assert!(types1.is_superset_of(&types2));
+        assert!(!types2.is_superset_of(&types1));
+        assert!(!types3.is_subset_of(&types1));
+        assert!(types2.is_subset_of(&types3));
+    }
+
+    #[test]
+    fn new_archetype() {
+        let mut entities = Entities::new();
+        let entity = entities.create();
+
+        let set = PearlSetBuilder::new()
+            .add(Type1(1))
+            .add(Type2(2))
+            .add(Type3(3))
+            .build();
+
+        let types = set.types().clone();
+        let archetype = Archetype::new(entity, set);
+        assert!(archetype.entities.len() == 1);
+        assert!(archetype.pearls.len() == 3);
+        assert!(types == archetype.types);
+    }
+
+    #[test]
+    fn insert_archetype() {
+        let mut entities = Entities::new();
+        let entity1 = entities.create();
+        let entity2 = entities.create();
+        let set1 = PearlSetBuilder::new()
+            .add(Type1(1))
+            .add(Type2(2))
+            .add(Type3(3))
+            .build();
+        let set2 = PearlSetBuilder::new()
+            .add(Type1(1))
+            .add(Type2(2))
+            .add(Type3(3))
+            .build();
+
+        let mut archetype = Archetype::new(entity1, set1);
+        archetype.insert(entity2, set2);
+        assert!(archetype.entities.len() == 2);
+        assert!(archetype.pearls.len() == 3);
+        assert!(archetype.pearls.get_index(0).unwrap().1.len() == 2);
+        assert!(archetype.pearls.get_index(1).unwrap().1.len() == 2);
+        assert!(archetype.pearls.get_index(2).unwrap().1.len() == 2);
+    }
+
+    #[test]
+    fn delete_from_archetype() {
+        let mut entities = Entities::new();
+        let entity1 = entities.create();
+        let entity2 = entities.create();
+        let set1 = PearlSetBuilder::new()
+            .add(Type1(1))
+            .add(Type2(2))
+            .add(Type3(3))
+            .build();
+        let set2 = PearlSetBuilder::new()
+            .add(Type1(4))
+            .add(Type2(5))
+            .add(Type3(6))
+            .build();
+
+        let mut archetype = Archetype::new(entity1, set1);
+        archetype.insert(entity2, set2);
+        archetype.destroy(&entity1);
+        assert!(archetype.entities.len() == 1);
+        assert!(archetype.pearls.len() == 3);
+        assert!(archetype.pearls.get_index(0).unwrap().1.len() == 1);
+        assert!(archetype.pearls.get_index(1).unwrap().1.len() == 1);
+        assert!(archetype.pearls.get_index(2).unwrap().1.len() == 1);
+
+        let type1_vec = archetype.pearls.get(&TypeId::of::<Type1>()).unwrap();
+        assert!(type1_vec.get::<Type1>(0).unwrap().0 == 4);
+
+        let type2_vec = archetype.pearls.get(&TypeId::of::<Type2>()).unwrap();
+        assert!(type2_vec.get::<Type2>(0).unwrap().0 == 5);
+
+        let type3_vec = archetype.pearls.get(&TypeId::of::<Type3>()).unwrap();
+        assert!(type3_vec.get::<Type3>(0).unwrap().0 == 6);
+    }
+
+    #[test]
+    fn remove_from_archetype() {
+        let mut entities = Entities::new();
+        let entity1 = entities.create();
+        let entity2 = entities.create();
+        let set1 = PearlSetBuilder::new()
+            .add(Type1(1))
+            .add(Type2(2))
+            .add(Type3(3))
+            .build();
+        let set2 = PearlSetBuilder::new()
+            .add(Type1(4))
+            .add(Type2(5))
+            .add(Type3(6))
+            .build();
+
+        let set2_types = set2.types().clone();
+        let mut archetype = Archetype::new(entity1, set1);
+        archetype.insert(entity2, set2);
+
+        let set2 = archetype.remove(&entity2).unwrap();
+        assert!(set2.types() == &set2_types);
+
+        assert!(archetype.entities.len() == 1);
+        assert!(archetype.pearls.len() == 3);
+        assert!(archetype.pearls.get_index(0).unwrap().1.len() == 1);
+        assert!(archetype.pearls.get_index(1).unwrap().1.len() == 1);
+        assert!(archetype.pearls.get_index(2).unwrap().1.len() == 1);
+
+        let type1_imp = set2.pearls.get(&TypeId::of::<Type1>()).unwrap();
+        assert!(type1_imp.downcast_ref::<Type1>().unwrap().0 == 4);
+
+        let type2_imp = set2.pearls.get(&TypeId::of::<Type2>()).unwrap();
+        assert!(type2_imp.downcast_ref::<Type2>().unwrap().0 == 5);
+
+        let type3_imp = set2.pearls.get(&TypeId::of::<Type3>()).unwrap();
+        assert!(type3_imp.downcast_ref::<Type3>().unwrap().0 == 6);
     }
 }
