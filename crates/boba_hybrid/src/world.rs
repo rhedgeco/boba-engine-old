81d946432e8358c8729bd6b9065cf6d035afeb85
Ryan Hedgecock <rhedgeco@gmail.com>
2023-03-28 18:22:37 -0700

    added world impl

--- crates/boba_hybrid/src/world.rs
+++ crates/boba_hybrid/src/world.rs
@@ -1,6 +1,14 @@
 use crate::{pearls::PearlCollection, BobaResources};
 
+#[derive(Default)]
 pub struct World {
     pub pearls: PearlCollection,
     pub resources: BobaResources,
+}
+
+impl World {
+    #[inline]
+    pub fn new() -> Self {
+        Self::default()
+    }
 }
