9ecb0e2092d7b749863b0a874d5a7f306e91a24d
Ryan Hedgecock <rhedgeco@gmail.com>
2023-03-28 18:07:08 -0700

    improved resource api

--- crates/boba_hybrid/src/resources.rs
+++ crates/boba_hybrid/src/resources.rs
@@ -2,17 +2,29 @@ use std::any::{Any, TypeId};
 
 use hashbrown::HashMap;
 
-trait ResourceAccessor {
+trait ResourceManager {
+    fn insert<T: 'static>(&mut self, resource: T);
     fn get<T: 'static>(&self) -> Option<&T>;
     fn get_mut<T: 'static>(&mut self) -> Option<&mut T>;
+    fn remove<T: 'static>(&mut self) -> Option<T>;
 }
 
 #[derive(Default)]
-pub struct Resources {
+pub struct BobaResources {
     resources: HashMap<TypeId, Box<dyn Any>>,
 }
 
-impl ResourceAccessor for Resources {
+impl BobaResources {
+    pub fn new() -> Self {
+        Self::default()
+    }
+}
+
+impl ResourceManager for BobaResources {
+    fn insert<T: 'static>(&mut self, resource: T) {
+        self.resources.insert(TypeId::of::<T>(), Box::new(resource));
+    }
+
     fn get<T: 'static>(&self) -> Option<&T> {
         let any = self.resources.get(&TypeId::of::<T>())?;
         Some(any.downcast_ref::<T>().unwrap())
@@ -22,14 +34,9 @@ impl ResourceAccessor for Resources {
         let any = self.resources.get_mut(&TypeId::of::<T>())?;
         Some(any.downcast_mut::<T>().unwrap())
     }
-}
 
-impl Resources {
-    pub fn new() -> Self {
-        Self::default()
-    }
-
-    pub fn insert<T: 'static>(&mut self, resource: T) {
-        self.resources.insert(TypeId::of::<T>(), Box::new(resource));
+    fn remove<T: 'static>(&mut self) -> Option<T> {
+        let any = self.resources.remove(&TypeId::of::<T>())?;
+        Some(*any.downcast::<T>().unwrap())
     }
 }
